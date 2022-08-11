652698d503abfff29592ddaebc116f069fb06cf5
unknown <temple0715@gmail.com>
2022-08-11 18:32:09 -0500

    genre seed completed

--- seeds/genreData.js
+++ seeds/genreData.js
@@ -1,1 +1,17 @@
-const {}
+const { Genre } = require('../models')
+
+const genreData = [
+    {
+        name: 'Rock'
+    },
+    {
+        name: 'Pop'
+    },
+    {
+        name: 'rap'
+    }
+]
+
+const seedGenre = () => Genre.bulkCreate(genreData);
+
+module.exports = seedGenre;
