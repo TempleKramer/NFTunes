0be7295ec7f54c5184ba2c2dc4825fae5ffecf6c
unknown <temple0715@gmail.com>
2022-08-16 18:39:45 -0500

    updated html routes require in server.js

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
-const path = require('path');
 const express = require('express')
-const sequelize = require('./config/connection');
-
-// const Artist = require('./models/Artist')
-// const Genre = require('./models/Genre')
-// const User = require('./models/User')
-
-const models = require('./models')
-
 
 const PORT = process.env.PORT || 3001
 const app = express()
-const htmlRoutes = require('./routes/htmlRoutes')
+const htmlRoutes = require('./controllers/routes/htmlRoutes')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -19,4 +10,6 @@ app.use(express.static('public'))
 
 app.use('/', htmlRoutes)
 
-sequelize.sync({ force: true }).then(() => {    app.listen(PORT, () => console.log('Now listening'));   });
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`)
+})
