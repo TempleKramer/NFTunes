bdc950b1bf612251b9e580ec7979e066149ed56e
Inez  Mercedes Jimenez <ijim9182@yahoo.com>
2022-08-15 19:35:49 -0500

    Adding onclick event

---
+++ public/js/script.js
@@ -0,0 +1,19 @@
+document.getElementById("search-button").onclick = function() {
+    let searchedValue = document.querySelector("#search-bar")
+    let artistData = searchedValue.value
+    let genreData = searchedValue.value
+    //passing artist name and genres to search function
+    search(artistData)
+    search(genreData)
+}
+
+let search = (artistData) => {
+    //now retrieving data
+    let name = data.name
+    let nameArtist = document.createElement("artist-title")
+    nameArtist.innerHTML = name
+    console.log(name_artist)
+    document.querySelector(".name").innerText = "name_artist";
+    
+    
+}
