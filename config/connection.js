6bdc8ed88589ae423e75ece5f9475d4db12e846b
Seleste <104727784+smillkaa@users.noreply.github.com>
2022-08-18 10:19:15 -0700

    changed genre get route

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 const Sequelize = require('sequelize');
-require('dotenv').config();
+// require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  // process.env.DB_NAME,
+  // process.env.DB_USER,
+  // process.env.DB_PASSWORD,
   {
     host: 'localhost',
     dialect: 'mysql',
