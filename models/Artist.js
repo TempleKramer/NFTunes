80cf3b809949ccdd80d4bb88be8c21b92bd49424
Temple Kramer <104111497+TempleKramer@users.noreply.github.com>
2022-08-18 15:12:14 -0500

    Update Artist.js

--- models/Artist.js
+++ models/Artist.js
@@ -4,44 +4,52 @@ const sequelize = require('../config/connection');
 class Artist extends Model {}
 
 Artist.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    artist: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-   
-    filename: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    description: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    gallery_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'genre',
-        key: 'id',
-      },
-    },
-  },
-  {
-    sequelize,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'artist',
-  }
-);
+    {
+        id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        awards: {
+          type: DataTypes.STRING,
+          allowNull: true,
 
-module.exports = Artist;
+        },
+        albumsSold: {
+          type: DataTypes.STRING,
+          allowNull: true, 
+
+        },
+        singlesSold: {
+          type: DataTypes.STRING,
+          allowNull: true,
+        
+        },
+        // genre_id: {
+        //     type: DataTypes.STRING,
+        //     references: {
+        //       model: 'genre',
+        //       key: 'id'
+        //     }
+        //   }
+    },  
+    {
+
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+
+      sequelize,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'genre',
+
+    }
+)
+
+module.exports = Artist
