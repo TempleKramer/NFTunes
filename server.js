7392b860dc0f95a628d352e7d8080b1774174e55
Inez  Mercedes Jimenez <ijim9182@yahoo.com>
2022-08-18 13:51:58 -0500

    testing npm run

---
+++ server.js
@@ -0,0 +1,21 @@
+const exphbs = require('express-handlebars');
+
+const express = require('express')
+
+const PORT = process.env.PORT || 3001
+const app = express()
+const routes = require('./controllers')
+
+const hbs = exphbs.create();
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars'); 
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static('public'))
+
+app.use(routes)
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`)
+});
